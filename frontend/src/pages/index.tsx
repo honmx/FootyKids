b4ba569fbb59c8fe40463a2ef4b3b0ce9851e4f1
honmx <max.klim2004@mail.ru>
2023-07-25 13:29:47 +0500

    issue fixed

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -3,7 +3,7 @@ import { NextPage } from "next";
 import MainBanner from "@/components/MainBanner";
 import AboutUs from "@/components/AboutUs";
 import { Stack } from "@mui/material";
-import { NextPageWithLayout } from "@/types/INextPageWithLayout";
+import { INextPageWithLayout } from "@/types/INextPageWithLayout";
 import Layout from "@/components/Layout/Layout";
 import Header from "@/components/Layout/Header";
 import Footer from "@/components/Layout/Footer";
@@ -13,7 +13,7 @@ interface Props {
 
 }
 
-const HomePage: NextPageWithLayout<Props> = ({ }) => {
+const HomePage: INextPageWithLayout<Props> = ({ }) => {
 
   return (
     <>
