d856da9efae36aeed5be59e0a194ce883a1fbf69
honmx <max.klim2004@mail.ru>
2023-08-11 13:25:56 +0500

    change useLayoutEffect to useEffect

--- frontend/src/hooks/useResize.ts
+++ frontend/src/hooks/useResize.ts
@@ -1,9 +1,9 @@
-import { useEffect, useLayoutEffect, useState } from "react"
+import { useEffect, useState } from "react"
 
 export const useResize = (width: number) => {
   const [isSmaller, setIsSmaller] = useState<boolean>(false);
 
-  useLayoutEffect(() => {
+  useEffect(() => {
     const handleResize = () => {
       setIsSmaller(window.innerWidth < width);
     }
